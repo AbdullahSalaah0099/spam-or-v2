243ae5638aec72adba7c0ff9272bbae9b99a01eb
Abdullah Salah <95449686+AbdullahSalaah0099@users.noreply.github.com>
2022-05-24 09:14:43 +0200

    Update spam orange v2.py

--- spam orange v2.py
+++ spam orange v2.py
@@ -5,7 +5,7 @@ from time import sleep
 print ('\033[1;92mclick on this link to get password👇')
 #sleep (0.1)
 print ()
-link1="\033[1;93m https://miklpro.com/L2Rr8G"
+link1="\033[1;93m https://miklpro.com/dppWzJc"
 print (link1)
 #sleep (1)
 print ()
@@ -16,7 +16,7 @@ sleep (1)
 soup=BS(rrr,'html.parser')
 lxc=(soup.find('div',{'class':'body-display'})).text'''
 
-if password=="Abdullah8438":
+if password=="Abdullah7020":
     print ()
     print ('\033[1;96m》True Password《')
     sleep (1)
