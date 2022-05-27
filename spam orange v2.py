c410207f9b1984e1e5ce3d611641f9fdb6697057
Abdullah Salah <95449686+AbdullahSalaah0099@users.noreply.github.com>
2022-05-27 10:11:37 +0200

    Update spam orange v2.py

--- spam orange v2.py
+++ spam orange v2.py
@@ -16,7 +16,7 @@ sleep (1)
 soup=BS(rrr,'html.parser')
 lxc=(soup.find('div',{'class':'body-display'})).text'''
 
-if password=="Abdullah7655":
+if password=="Abdullah1115":
     print ()
     print ('\033[1;96m》True Password《')
     sleep (1)
