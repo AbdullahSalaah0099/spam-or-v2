164d963115e2fba1327be32cdb3d8b9db20c3246
Abdullah Salah <95449686+AbdullahSalaah0099@users.noreply.github.com>
2022-06-09 07:15:41 +0200

    Create spam orange v2.py

--- spam orange v2.py
+++ spam orange v2.py
@@ -5,14 +5,14 @@ from time import sleep
 print ('\033[1;92mclick on this link to get password👇')
 #sleep (0.1)
 print ()
-link1="\033[1;93m https://miklpro.com/sF9r"
+link1="\033[1;93m https://miklpro.com/scs4Je"
 print (link1)
 #sleep (1)
 print ()
 password=input ('\033[1;92m》Enter Password Script :  \033[1;96m')
 sleep (1)
 
-rrr=requests.get('https://pastelink.net/e2juzm8f').text
+rrr=requests.get('https://pastelink.net/i34r55hl').text
 soup=BS(rrr,'html.parser')
 lxc=(soup.find('div',{'class':'body-display'})).text
 
